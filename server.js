84eaf4a60de679fe27a0317b3a21f702a6d8e95b
chauthutran <cthutran@gmail.com>
2024-08-28 23:53:36 +0900

    Updated

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 require('dotenv').config({ path: '.env.local' });
 require('./services/scheduler'); // Import the scheduler to start the cron job
 
@@ -10,3 +12,6 @@ console.log('Scheduler started. Waiting for tasks to run every 1 hour...');
 app.get('/', (req, res) => {
     res.send('Service is start!');
 });
+
+// Export the app for Vercel to use
+module.exports = app;
