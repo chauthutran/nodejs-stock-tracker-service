9a4888283e12e9da59ea6ca888dfc961a740bbe2
chauthutran <cthutran@gmail.com>
2024-08-29 10:18:13 +0900

    Update scheduler.js

--- services/scheduler.js
+++ services/scheduler.js
@@ -1,11 +1,6 @@
 const cron = require('node-cron');
 const { runNoticationSettings } = require('./stockService');
 
-// import {
-// 	runNoticationSettings
-// } from './stockService'; // Your stock checking and notification logic
-
-
 
 // Define the task to be run every 1 hour
 // cron.schedule('*/1 * * * *', async () => { // every 1 minute
